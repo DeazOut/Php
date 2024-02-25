c8b9e2020a516e0ec60fb88ad22b7495f3ec682e
DeazOut <88485210+DeazOut@users.noreply.github.com>
2024-02-25 13:05:19 +0300

    Create setpoint.php

---
+++ setpoint.php
@@ -0,0 +1,26 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+
+header('Access-Control-Allow-Credentials: true');
+
+header('Access-Control-Allow-Headers: Authorization, Origin, X-Requested-With, Accept, X-PINGOTHER, Content-Type');
+if (isset($_GET['x']) && isset($_GET['y'])){
+  $x = $_GET['x'];
+  $y = $_GET['y'];
+  $points = file_get_contents("points.json");
+  $pointsArray = json_decode($points, true);
+  $newPoint = [
+        'x' => $x,
+        'y' => $y,
+        'color' => 'black',
+    ];
+  $pointsArray[] = $newPoint;
+  $pointsjs = json_encode($pointsArray);
+  file_put_contents("points.json", $pointsjs);
+} else {
+  error_log("Nice try");  
+}
+?>
